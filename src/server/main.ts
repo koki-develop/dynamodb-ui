104c76d6b2926a661715f14f947ac04e47a093d9
koki-develop <kou.pg.0131@gmail.com>
2024-03-01 06:32:58 +0900

    Add express server with vite and hello world endpoint

---
+++ src/server/main.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { createServer } from "vite";
+
+(async () => {
+  const app = express();
+  const vite = await createServer({
+    server: { middlewareMode: true },
+  });
+  app.get("/api/hello", (_req, res) => {
+    res.status(200).json({ message: "Hello, world!" });
+  });
+
+  app.use(vite.middlewares);
+
+  console.log("Server listening on http://localhost:3000");
+  app.listen(3000);
+})();
