831d7dea84dfd3cbb95c005a87c81841729b9e78
koki-develop <kou.pg.0131@gmail.com>
2024-03-01 22:09:59 +0900

    Create pages directory

--- src/client/router.tsx
+++ src/client/router.tsx
@@ -1,5 +1,5 @@
 import Layout from "@/client/components/Layout";
-import TablesPage from "@/client/components/TablesPage/TablesPage";
+import TablesPage from "@/client/components/pages/TablesPage/TablesPage";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
