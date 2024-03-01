7f4f0aba2265a946bc3f2b3b6c4ce9b8a10d4aec
koki-develop <kou.pg.0131@gmail.com>
2024-03-01 18:07:42 +0900

    Add QueryClient and TablesPage to App component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,22 @@
+import TablesPage from "@/client/components/TablesPage/TablesPage";
 import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 0,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function App() {
-  return <MantineProvider>hello world</MantineProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <TablesPage />
+      </MantineProvider>
+    </QueryClientProvider>
+  );
 }
