dc2e9ea06a03e1b947e5fcddb0fd8510b1ad7eeb
koki-develop <kou.pg.0131@gmail.com>
2024-03-01 09:41:22 +0900

    Add root directory path to Vite server config

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { createServer } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const root = path.resolve(__dirname, "../");
 
 (async () => {
   const app = express();
   const vite = await createServer({
     server: { middlewareMode: true },
     logLevel: import.meta.env.PROD ? "silent" : "info",
+    root,
   });
   app.get("/api/hello", (_req, res) => {
     res.status(200).json({ message: "Hello, world!" });
