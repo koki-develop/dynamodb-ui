c5ee2967b3c87b737c2a3e400fb53ac321a7b125
koki-develop <kou.pg.0131@gmail.com>
2024-03-01 18:13:12 +0900

    Add error handling to server routes

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,10 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import express from "express";
+import express, {
+  NextFunction,
+  Request,
+  RequestHandler,
+  Response,
+} from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { createServer } from "vite";
@@ -10,6 +15,12 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const root = import.meta.env.PROD
   ? path.resolve(__dirname, "../")
   : process.cwd();
+
+const wrapHandler = (handler: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(handler(req, res, next)).catch((error) => next(error));
+  };
+};
 
 (async () => {
   const vite = await createServer({
@@ -23,22 +34,39 @@ const root = import.meta.env.PROD
   const itemsController = new ItemsController(dbClient);
 
   const router = express.Router();
-  router.get("/tables", tablesController.listTables.bind(tablesController));
-  router.get("/tables/:name", tablesController.getTable.bind(tablesController));
-  router.post("/tables", tablesController.createTable.bind(tablesController));
+  router.get(
+    "/tables",
+    wrapHandler(tablesController.listTables.bind(tablesController)),
+  );
+  router.get(
+    "/tables/:name",
+    wrapHandler(tablesController.getTable.bind(tablesController)),
+  );
+  router.post(
+    "/tables",
+    wrapHandler(tablesController.createTable.bind(tablesController)),
+  );
   router.delete(
     "/tables/:name",
-    tablesController.deleteTable.bind(tablesController),
+    wrapHandler(tablesController.deleteTable.bind(tablesController)),
   );
   router.get(
     "/tables/:name/items",
-    itemsController.listItems.bind(itemsController),
+    wrapHandler(itemsController.listItems.bind(itemsController)),
+  );
+  router.use(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (error: Error, _req: Request, res: Response, _next: NextFunction) => {
+      console.error(error);
+      return res.status(500).json({ error: error });
+    },
   );
 
   const app = express();
 
   app.use(express.json());
   app.use("/api", router);
+
   app.use(vite.middlewares);
 
   console.log("Server listening on http://localhost:3000");
