750f7e5df3b317ca798c6d6731a1fab22f9fdc3e
koki-develop <kou.pg.0131@gmail.com>
2024-03-03 08:06:46 +0900

    Add TablesTable component and refactor TablesPage

---
+++ src/client/components/features/tables/TablesTable.tsx
@@ -0,0 +1,41 @@
+import { TableDescription } from "@aws-sdk/client-dynamodb";
+import { ActionIcon, Anchor, Paper, Table } from "@mantine/core";
+import { IconDots } from "@tabler/icons-react";
+import { Link } from "react-router-dom";
+
+export type TablesTableProps = {
+  tables: TableDescription[];
+};
+
+export default function TablesTable({ tables }: TablesTableProps) {
+  return (
+    <Paper shadow="xs" px="md">
+      <Table horizontalSpacing="md" verticalSpacing="md">
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Table Name</Table.Th>
+            <Table.Th>Item Count</Table.Th>
+            <Table.Th />
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {tables.map((table) => (
+            <Table.Tr key={table.TableName}>
+              <Table.Td>
+                <Anchor component={Link} to={`/table/${table.TableName}`}>
+                  {table.TableName}
+                </Anchor>
+              </Table.Td>
+              <Table.Td>{table.ItemCount?.toLocaleString()}</Table.Td>
+              <Table.Td align="right">
+                <ActionIcon variant="transparent" c="gray">
+                  <IconDots />
+                </ActionIcon>
+              </Table.Td>
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+    </Paper>
+  );
+}
--- src/client/components/pages/TablesPage/TablesPage.tsx
+++ src/client/components/pages/TablesPage/TablesPage.tsx
@@ -1,10 +1,9 @@
 import Loader from "@/client/components/util/Loader";
 import Page from "@/client/components/util/Page";
 import { useTables } from "@/client/lib/tables";
-import { ActionIcon, Anchor, Box, Paper, Table, Text } from "@mantine/core";
-import { IconDots } from "@tabler/icons-react";
+import { Text } from "@mantine/core";
 import { useMemo } from "react";
-import { Link } from "react-router-dom";
+import TablesTable from "../../features/tables/TablesTable";
 
 export default function TablesPage() {
   const { data, isFetching, error } = useTables();
@@ -22,36 +21,7 @@ export default function TablesPage() {
 
   return (
     <Page>
-      <Box>
-        <Paper shadow="xs" px="md">
-          <Table horizontalSpacing="md" verticalSpacing="md">
-            <Table.Thead>
-              <Table.Tr>
-                <Table.Th>Table Name</Table.Th>
-                <Table.Th>Item Count</Table.Th>
-                <Table.Th />
-              </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody>
-              {tables.map((table) => (
-                <Table.Tr key={table.TableName}>
-                  <Table.Td>
-                    <Anchor component={Link} to={`/table/${table.TableName}`}>
-                      {table.TableName}
-                    </Anchor>
-                  </Table.Td>
-                  <Table.Td>{table.ItemCount?.toLocaleString()}</Table.Td>
-                  <Table.Td align="right">
-                    <ActionIcon variant="transparent" c="gray">
-                      <IconDots />
-                    </ActionIcon>
-                  </Table.Td>
-                </Table.Tr>
-              ))}
-            </Table.Tbody>
-          </Table>
-        </Paper>
-      </Box>
+      <TablesTable tables={tables} />
     </Page>
   );
 }
