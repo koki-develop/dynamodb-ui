e3b4f487dd528732d8e45eff40bfbaf8510c90c2
koki-develop <kou.pg.0131@gmail.com>
2024-03-03 22:42:27 +0900

    Enable minification during build process

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
