06624b611acc649a7d4730e8020efa1ab0aba859
koki-develop <kou.pg.0131@gmail.com>
2024-10-25 22:59:28 +0900

    Remove unnecessary log

--- src/client/components/util/ErrorText.tsx
+++ src/client/components/util/ErrorText.tsx
@@ -1,16 +1,11 @@
 import { Alert, Text } from "@mantine/core";
 import { IconExclamationCircle } from "@tabler/icons-react";
-import { useEffect } from "react";
 
 export type ErrorTextProps = {
   error?: Error | null;
 };
 
 export default function ErrorAlert({ error }: ErrorTextProps) {
-  useEffect(() => {
-    if (error) console.error(error);
-  }, [error]);
-
   if (!error) return null;
 
   return (
