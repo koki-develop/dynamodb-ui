70ca12cec456544b4eb6307549d12fe1f024962b
koki-develop <kou.pg.0131@gmail.com>
2024-10-25 23:15:22 +0900

    Refactor TablePage

--- src/client/components/pages/TablePage/TablePage.tsx
+++ src/client/components/pages/TablePage/TablePage.tsx
@@ -1,37 +1,37 @@
 import ItemsTable from "@/client/components/features/items/ItemsTable";
 import TableSummary from "@/client/components/features/tables/TableSummary";
-import Loader from "@/client/components/util/Loader";
 import Page from "@/client/components/util/Page";
-import { useTable } from "@/client/lib/tables";
-import { Box, Stack, Title } from "@mantine/core";
-import { useMemo } from "react";
+import { useTable } from "@/client/lib/hooks";
+import { Box, Loader, Stack, Title } from "@mantine/core";
 import { useParams } from "react-router-dom";
 
 export default function TablePage() {
-  const name = useParams<{ name: string }>().name as string;
-  const { data, isLoading, error } = useTable(name);
+  const name = useParams().name as string;
+  const { data, isLoading, error } = useTable({ TableName: name });
 
-  const breadcrumbs = useMemo(
-    () => [
-      { title: "Tables", to: "/" },
-      { title: name, to: `/table/${name}` },
-    ],
-    [name],
-  );
+  const breadcrumbs = [
+    { title: "Tables", to: "/" },
+    { title: name, to: `/table/${name}` },
+  ];
 
   return (
     <Page breadcrumbs={breadcrumbs} error={error}>
-      {isLoading && <Loader />}
-      {data && (
+      {isLoading && (
+        <Box className="flex justify-center" py="md">
+          <Loader />
+        </Box>
+      )}
+
+      {data?.Table && (
         <Stack>
-          <Box>
-            <Title order={2} size="h3">
-              {data.Table.TableName}
-            </Title>
-          </Box>
+          <Title order={2} size="h3">
+            {data.Table.TableName}
+          </Title>
+
           <Box>
             <TableSummary table={data.Table} />
           </Box>
+
           <Box>
             <ItemsTable table={data.Table} />
           </Box>
