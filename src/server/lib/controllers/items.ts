3efda57a5b82e4bbdeaee4004b31f7fb4c0e287d
koki-develop <kou.pg.0131@gmail.com>
2024-10-25 16:38:26 +0900

    Refactor ItemsController to simplify input handling

--- src/server/lib/controllers/items.ts
+++ src/server/lib/controllers/items.ts
@@ -1,23 +1,19 @@
+import { deserializeAttribute, serializeAttribute } from "@/shared/util";
 import { type DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 import type { Request, Response } from "express";
-import { listItemsInputSchema } from "../../../shared/types";
-import { deserializeAttribute, serializeAttribute } from "../../../shared/util";
 
 export class ItemsController {
   constructor(private readonly dbClient: DynamoDBClient) {}
 
   async listItems(req: Request, res: Response) {
-    const result = listItemsInputSchema.safeParse(req.body);
-    if (!result.success) return res.status(400).json(result.error);
-    const { data: input } = result;
+    const input = JSON.parse(req.body);
 
     const response = await this.dbClient.send(
       new ScanCommand({
-        TableName: input.name,
-        Limit: input.limit ?? 300,
+        ...input,
         ExclusiveStartKey:
-          input.exclusiveStartKey &&
-          deserializeAttribute(input.exclusiveStartKey),
+          input.ExclusiveStartKey &&
+          deserializeAttribute(input.ExclusiveStartKey),
       }),
     );
 
