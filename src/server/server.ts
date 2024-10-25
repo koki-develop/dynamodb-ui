fcbe37d793aa0fed22e0fbfbaebff28dbcf5cfb8
koki-develop <kou.pg.0131@gmail.com>
2024-10-25 18:13:25 +0900

    Add port flag

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { program } from "commander";
 import express, {
   type NextFunction,
   type Request,
@@ -15,13 +16,17 @@ const root = import.meta.env.PROD
   ? path.resolve(__dirname, "../client")
   : process.cwd();
 
-const wrapHandler = (handler: RequestHandler) => {
+const _wrapHandler = (handler: RequestHandler) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(handler(req, res, next)).catch((error) => next(error));
   };
 };
 
-(async () => {
+type Options = {
+  port: number;
+};
+
+const main = async (options: Options) => {
   const vite = await createServer({
     server: { middlewareMode: true },
     logLevel: import.meta.env.PROD ? "silent" : "info",
@@ -34,23 +39,23 @@ const wrapHandler = (handler: RequestHandler) => {
   const router = express.Router();
   router.post(
     "/listTables",
-    wrapHandler((req, res) => controller.listTables(req, res)),
+    _wrapHandler((req, res) => controller.listTables(req, res)),
   );
   router.post(
     "/describeTable",
-    wrapHandler((req, res) => controller.describeTable(req, res)),
+    _wrapHandler((req, res) => controller.describeTable(req, res)),
   );
   router.post(
     "/createTable",
-    wrapHandler((req, res) => controller.createTable(req, res)),
+    _wrapHandler((req, res) => controller.createTable(req, res)),
   );
   router.post(
     "/deleteTable",
-    wrapHandler((req, res) => controller.deleteTable(req, res)),
+    _wrapHandler((req, res) => controller.deleteTable(req, res)),
   );
   router.post(
     "/scan",
-    wrapHandler((req, res) => controller.scan(req, res)),
+    _wrapHandler((req, res) => controller.scan(req, res)),
   );
 
   router.use(
@@ -67,6 +72,18 @@ const wrapHandler = (handler: RequestHandler) => {
 
   app.use(vite.middlewares);
 
-  console.log("Server listening on http://localhost:3000");
-  app.listen(3000);
-})();
+  const port = Number(options.port);
+  if (Number.isNaN(port)) {
+    throw new Error(`Invalid port: ${options.port}`);
+  }
+
+  console.log(`Server listening on http://localhost:${port}`);
+  app.listen(port);
+};
+
+program.option("-p, --port <port>", "Port to listen on", "3000").parse();
+program.parse();
+
+const options = program.opts<Options>();
+
+main(options);
