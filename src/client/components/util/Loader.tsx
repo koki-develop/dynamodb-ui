ba5d60e5f3867a485a1210edfe6e6b8cddc9492e
koki-develop <kou.pg.0131@gmail.com>
2024-10-25 23:27:26 +0900

    Remove util/Loader.tsx

--- src/client/components/util/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Loader as MantineLoader } from "@mantine/core";
-
-export default function Loader() {
-  return (
-    <Box className="flex justify-center" py="md">
-      <MantineLoader />
-    </Box>
-  );
-}
