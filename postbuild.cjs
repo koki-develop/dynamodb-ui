8e4994a359fe622b0e42006044f81ae583e4e0ee
koki-develop <kou.pg.0131@gmail.com>
2024-10-26 09:19:21 +0900

    chore: Add node: prefix

--- postbuild.cjs
+++ postbuild.cjs
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs");
+const path = require("node:path");
 
 const bin = path.resolve(__dirname, "./dist/server/server.js");
 const shebang = "#!/usr/bin/env node\n\n";
